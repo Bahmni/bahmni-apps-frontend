d480f37ff992e5b75a9f461209380aa410485727
MOHANKUMAR T <mohan13081999@gmail.com>
2025-03-19 13:59:58 +0530

    Fix. URI Malformed Exception

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,11 @@ import './styles/index.scss';
 // Register service worker for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
+    // Use a relative path that will work regardless of the base URL
+    const swUrl = new URL('/service-worker.js', window.location.origin).pathname;
+    
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register(swUrl)
       .then((registration) => {
         console.log('SW registered: ', registration);
       })
