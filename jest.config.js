bdd13535f8ceb24a30b1fd25750de23e62d77f72
MOHANKUMAR T <mohan13081999@gmail.com>
2025-03-20 08:45:04 +0530

    BN-35 | Add. Jest and React Testing library for unit tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
