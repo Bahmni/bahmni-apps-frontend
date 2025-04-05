87c0d7ddf2a536388f199e5365c8cc911e678476
arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>
2025-04-05 13:56:31 +0530

    Apply translations.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Grid, Column, Section } from '@carbon/react';
 import PatientDetails from '@components/patient/PatientDetails';
 import ConditionsTable from '@components/conditions/ConditionsTable';
@@ -6,15 +6,17 @@ import AllergiesTable from '@components/allergies/AllergiesTable';
 
 const HomePage: React.FC = () => {
   return (
-    <Section>
-      <Grid>
-        <Column lg={16} md={8} sm={4}>
-          <PatientDetails />
-          <AllergiesTable />
-          <ConditionsTable />
-        </Column>
-      </Grid>
-    </Section>
+    <Suspense fallback="loading">
+      <Section>
+        <Grid>
+          <Column lg={16} md={8} sm={4}>
+            <PatientDetails />
+            <AllergiesTable />
+            <ConditionsTable />
+          </Column>
+        </Grid>
+      </Section>
+    </Suspense>
   );
 };
 
