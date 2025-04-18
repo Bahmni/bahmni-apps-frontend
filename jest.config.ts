9d1a67e73ed7df864c151582b82468618e73f63d
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-04-18 12:49:09 +0530

    BN-41 | Add. Base Path Alias

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,7 @@ const config: Config.InitialOptions = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.ts',
+    '@/(.*)$': ['<rootDir>/src/$1'],
     '@components/(.*)$': ['<rootDir>/src/components/$1'],
     '@contexts/(.*)$': ['<rootDir>/src/contexts/$1'],
     '@constants/(.*)$': ['<rootDir>/src/constants/$1'],
