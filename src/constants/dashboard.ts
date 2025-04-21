4f60c44c9cf7f54fd6ac8cd6297bbb2f59015570
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-04-21 23:23:52 +0530

    BN-37 | Refactor. Move Config URL To app

--- src/constants/dashboard.ts
+++
@@ -1,5 +0,0 @@
-export const DEPARTMENT_CONFIG_URL = (departmentURL: string) =>
-  `/bahmni_config/openmrs/apps/clinical/dashboards/${departmentURL}`;
-
-export const DASHBOARD_CONFIG_URL =
-  '/bahmni_config/openmrs/apps/clinical/dashboards/app.json';
