5fd808e2f0f924748936dbb852967f9470169d43
MOHANKUMAR T <mohan13081999@gmail.com>
2025-05-08 08:15:07 +0530

    BN-19 | Add. Render existing display controls based on section name

--- src/components/clinical/dashboardSection/DashboardSection.tsx
+++ src/components/clinical/dashboardSection/DashboardSection.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { Tile } from '@carbon/react';
 import { DashboardSectionConfig } from '@types/dashboardConfig';
 import * as styles from './styles/DashboardSection.module.scss';
+import AllergiesTable from '@/displayControls/allergies/AllergiesTable';
+import ConditionsTable from '@/displayControls/conditions/ConditionsTable';
 
 export interface DashboardSectionProps {
   section: DashboardSectionConfig;
 }
 
+//TODO: Refactor this to depend on Controls configuration
+const renderSectionContent = (section: DashboardSectionConfig) => {
+  switch (section.name) {
+    case 'Allergies':
+      return <AllergiesTable />;
+    case 'Conditions':
+      return <ConditionsTable />;
+    default:
+      return null;
+  }
+};
 /**
  * DashboardSection component that renders a single dashboard section as a Carbon Tile
  *
@@ -18,6 +31,7 @@ const DashboardSection: React.FC<DashboardSectionProps> = ({ section }) => {
     <div id={`section-${section.name}`}>
       <Tile>
         <p className={styles.sectionTitle}>{section.name}</p>
+        {renderSectionContent(section)}
       </Tile>
     </div>
   );
