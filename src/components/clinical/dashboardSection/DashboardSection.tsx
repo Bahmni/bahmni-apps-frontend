a529c4a5aea76083bce504e72382276592c864a5
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2025-05-08 15:56:29 +0530

    Bn 19 dashboard sections (#17)
    * BN-19 | Add. Dashboard Container and Section Components
    
    * BN-19 | Refactor. Type names to avoid conflicts with component names
    
    * BN-19 | Add. Render existing display controls based on section name
    
    * BN-19 | Add. i18n for dashboard section title
    
    * BN-19 | Refactor. Move Sidebar Actions Into A Custom Hook
    
    ---------
    
    Co-authored-by: Rahul Ramesh <rahul.ramesh@thoughtworks.com>

---
+++ src/components/clinical/dashboardSection/DashboardSection.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Tile } from '@carbon/react';
+import { DashboardSectionConfig } from '@types/dashboardConfig';
+import * as styles from './styles/DashboardSection.module.scss';
+import AllergiesTable from '@/displayControls/allergies/AllergiesTable';
+import ConditionsTable from '@/displayControls/conditions/ConditionsTable';
+import { useTranslation } from 'react-i18next';
+
+export interface DashboardSectionProps {
+  section: DashboardSectionConfig;
+}
+
+//TODO: Refactor this to depend on Controls configuration
+const renderSectionContent = (section: DashboardSectionConfig) => {
+  switch (section.name) {
+    case 'Allergies':
+      return <AllergiesTable />;
+    case 'Conditions':
+      return <ConditionsTable />;
+    default:
+      return null;
+  }
+};
+/**
+ * DashboardSection component that renders a single dashboard section as a Carbon Tile
+ *
+ * @param {DashboardSectionProps} props - Component props
+ * @returns {React.ReactElement} The rendered component
+ */
+const DashboardSection: React.FC<DashboardSectionProps> = ({ section }) => {
+  const { t } = useTranslation();
+  return (
+    <div id={`section-${section.name}`}>
+      <Tile>
+        <p className={styles.sectionTitle}>
+          {t(section.translationKey || section.name)}
+        </p>
+        {renderSectionContent(section)}
+      </Tile>
+    </div>
+  );
+};
+
+export default DashboardSection;
