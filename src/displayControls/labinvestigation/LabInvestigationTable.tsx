636f81cba096dac4d8b3ed197f8e6ec25aea0c79
Dev Singh <88476780+devsingh05298@users.noreply.github.com>
2025-05-09 16:13:01 +0530

    BN-49 | Moved test details and data table in separate components

--- src/displayControls/labinvestigation/LabInvestigationTable.tsx
+++ src/displayControls/labinvestigation/LabInvestigationTable.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { ExpandableDataTable } from '@components/common/expandableDataTable/ExpandableDataTable';
-import { Accordion, AccordionItem, DataTable, IconTab, Table, TableBody, TableCell, TableHead, TableHeader, TableRow, Tag, Tile } from '@carbon/react';
+import { Accordion, AccordionItem, DataTable, IconTab, Table, TableBody, TableCell, TableHead, TableHeader, TableRow, Tag, TextInput, Tile } from '@carbon/react';
 import BahmniIcon from "@components/common/bahmniIcon/BahmniIcon";
 import { ICON_SIZE } from '@/constants/icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
 
 interface LabInvestigationTable {
     id: string;
@@ -36,59 +34,13 @@ const LabInvestigationTable: React.FC = () => {
     return (
         <div>
 
-            <Tile className="my-tile">
-                <div className="header-row" style={{ display: 'flex', alignItems: 'center' , backgroundColor:'white'}}>
-                    <h5 style={{ display: 'inline', marginRight: '1rem' }}>Thyroid function Test</h5>
-                    <span>Single Test</span>
-                    <Tag type="green" className="ml-2">Routine</Tag>
-                </div>
-
-                <div className="info-row" style={{ marginTop: '1rem',backgroundColor:'white' }}>
-                    <FontAwesomeIcon
-                        icon={['fas', 'user-md']}
-                        size={ICON_SIZE.XS}
-                        style={{ marginRight: '0.5rem' }}
-                    />
-                    <span style={{ marginLeft: '0.2rem' }}>Ordered by: Dr. Sarah Johnson</span>
-                    <FontAwesomeIcon
-                        icon={['fas', 'flask']}
-                        size={ICON_SIZE.XS}
-                        style={{ marginRight: '0.5rem' }}
-                    />
-                    <span style={{ marginLeft: '0.2rem' }}>Sample type: Blood</span>
-                </div>
-            </Tile>
-            <div style={{ marginTop: '1rem', color: '#525252' }}>
-                    <strong>Results pending...</strong>
-            </div>
-
             <Accordion>
                 <AccordionItem title="23-05-2023">
-
-                    <DataTable rows={mockLabInvestigations} headers={headers}>
-                        {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
-                            <Table {...getTableProps()}>
-                                <TableHead>
-                                    <TableRow>
-                                        {headers.map((headers) => (
-                                            <TableHeader {...getHeaderProps({ header: headers })} key={headers.key}>
-                                                {headers.header}
-                                            </TableHeader>
-                                        ))}
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {rows.map((row) => (
-                                        <TableRow {...getRowProps({ row })}>
-                                            {row.cells.map((cell) => (
-                                                <TableCell key={cell.id}>{cell.value}</TableCell>
-                                            ))}
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        )}
-                    </DataTable>
+                    <TestDetails />
+                    <Tile>
+                        <strong>Results pending...</strong>
+                    </Tile>
+                    <LabTestDetailsTable />
                 </AccordionItem>
             </Accordion>
         </div>
@@ -96,3 +48,65 @@ const LabInvestigationTable: React.FC = () => {
 };
 
 export default LabInvestigationTable;
+
+
+const TestDetails: React.FC = () => {
+    return (
+        <Tile style={{ width: '100%', paddingLeft: '2rem, 1rem', marginTop: '0.5rem', backgroundColor: 'white', borderRadius: '12px 12px 0px 0px' }}>
+            {/* Header Row */}
+            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '0.5rem' }}>
+                <h5 style={{ margin: 0, marginRight: '1rem' }}>Thyroid function Test</h5>
+                <span style={{ marginRight: '1rem' }}>Single Test</span>
+                <Tag type="green">Routine</Tag>
+            </div>
+
+            {/* Info Row */}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <FontAwesomeIcon
+                    icon={['fas', 'user-md']}
+                    size={ICON_SIZE.XS}
+                    style={{ marginLeft: '0.5rem', marginRight: '1rem' }}
+                />
+                <span style={{ marginRight: '1.5rem' }}>Ordered by: Dr. Sarah Johnson</span>
+                <FontAwesomeIcon
+                    icon={['fas', 'flask']}
+                    size={ICON_SIZE.XS}
+                    style={{ marginRight: '0.5rem' }}
+                />          <span>Sample type: Blood</span>
+            </div>
+        </Tile>
+    );
+};
+
+const LabTestDetailsTable: React.FC = () => {
+    return (
+        <DataTable rows={mockLabInvestigations} headers={headers}>
+            {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+                <Table {...getTableProps()}>
+                    <TableHead>
+                        <TableRow>
+                            {headers.map((headers) => (
+                                <TableHeader {...getHeaderProps({ header: headers })} key={headers.key}>
+                                    {headers.header}
+                                </TableHeader>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                
+                        </TableRow>
+                        {rows.map((row) => (
+                            <TableRow {...getRowProps({ row })}>
+                                {row.cells.map((cell) => (
+                                    <TableCell key={cell.id}>{cell.value}</TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            )}
+        </DataTable>
+    );
+}
+
