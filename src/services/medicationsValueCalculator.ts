d04eb160f5234573d7263fe7d3bf7f6eac6c8104
MOHANKUMAR T <mohan13081999@gmail.com>
2025-06-27 13:11:35 +0530

    BN-72 | Add. Methods to auto calculate default values for medication form controls

---
+++ src/services/medicationsValueCalculator.ts
@@ -0,0 +1,71 @@
+import { Concept } from '@/types/encounterConcepts';
+import { DurationUnitOption } from '@types/medication';
+import { DrugFormDefault, Frequency } from '@types/medicationConfig';
+import { Medication } from 'fhir/r4';
+
+export const getDefaultRoute = (
+  medication: Medication,
+  drugFormDefaults: Record<string, DrugFormDefault>,
+  routes: Concept[],
+): Concept | undefined => {
+  const drugForm = getDrugFormName(medication);
+  if (!drugForm) {
+    return undefined;
+  }
+  const routeName = drugFormDefaults[drugForm]?.route;
+  if (!routeName) {
+    return undefined;
+  }
+  return routes.find((route) => route.name === routeName);
+};
+
+export const getDefaultDosingUnit = (
+  medication: Medication,
+  drugFormDefaults: Record<string, DrugFormDefault>,
+  dosingUnits: Concept[],
+): Concept | undefined => {
+  const drugForm = getDrugFormName(medication);
+  if (!drugForm) {
+    return undefined;
+  }
+  const dosingUnitName = drugFormDefaults[drugForm]?.doseUnits;
+  if (!dosingUnitName) {
+    return undefined;
+  }
+  return dosingUnits.find((unit) => unit.name === dosingUnitName);
+};
+
+export const calculateTotalQuantity = (
+  dosage: number,
+  frequency: Frequency | null,
+  duration: number,
+  durationUnit: DurationUnitOption,
+): number => {
+  if (frequency && isImmediateFrequency(frequency)) {
+    return dosage;
+  } else if (
+    dosage <= 0 ||
+    duration <= 0 ||
+    !durationUnit ||
+    !frequency ||
+    !frequency.frequencyPerDay ||
+    frequency.frequencyPerDay === 0
+  ) {
+    return 0;
+  }
+  const frequencyMultiplier = frequency.frequencyPerDay;
+  const durationMultiplier = duration * durationUnit.daysMultiplier;
+  return Math.ceil(dosage * frequencyMultiplier * durationMultiplier);
+};
+
+const getDrugFormName = (medication: Medication): string | undefined => {
+  const medicationForm = medication?.form;
+  if (!medicationForm || !medicationForm.text) {
+    return undefined;
+  }
+  return medicationForm.text;
+};
+
+export const isImmediateFrequency = (frequency: Frequency): boolean => {
+  return frequency.uuid === '0';
+};
