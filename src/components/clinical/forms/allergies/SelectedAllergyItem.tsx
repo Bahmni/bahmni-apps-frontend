011728cb467205621baab5bd8c239d4d854fbc16
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-06-30 20:36:40 +0530

    BN-85 | Add. Note Link Option In Selected Allergy

--- src/components/clinical/forms/allergies/SelectedAllergyItem.tsx
+++ src/components/clinical/forms/allergies/SelectedAllergyItem.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Column, Grid, Dropdown, FilterableMultiSelect } from '@carbon/react';
+import React, { useState } from 'react';
+import {
+  Column,
+  Grid,
+  Dropdown,
+  FilterableMultiSelect,
+  Link,
+} from '@carbon/react';
 import { useTranslation } from 'react-i18next';
 import * as styles from './styles/SelectedAllergyItem.module.scss';
 import { Coding } from 'fhir/r4';
 import { ALLERGY_SEVERITY_CONCEPTS } from '@constants/concepts';
 import { AllergyInputEntry } from '@types/allergy';
 import { getCategoryDisplayName } from '@utils/allergy';
+import TextAreaWClose from '@components/common/textAreaWClose/TextAreaWClose';
 
 /**
  * Properties for a selected allergy item
  * @interface SelectedAllergyItemProps
  */
 export interface SelectedAllergyItemProps {
+  /** The allergy input entry containing all allergy data */
   allergy: AllergyInputEntry;
+  /** Available reaction concepts for the multiselect */
   reactionConcepts: Coding[];
+  /** Callback function to update allergy severity */
   updateSeverity: (allergyId: string, severity: Coding | null) => void;
+  /** Callback function to update allergy reactions */
   updateReactions: (allergyId: string, reactions: Coding[]) => void;
+  /** Callback function to update allergy note */
+  updateNote: (allergyId: string, note: string) => void;
 }
 
 /**
@@ -24,7 +37,13 @@ export interface SelectedAllergyItemProps {
  * @param {SelectedAllergyItemProps} props - Component props
  */
 const SelectedAllergyItem: React.FC<SelectedAllergyItemProps> = React.memo(
-  ({ allergy, reactionConcepts, updateSeverity, updateReactions }) => {
+  ({
+    allergy,
+    reactionConcepts,
+    updateSeverity,
+    updateReactions,
+    updateNote,
+  }) => {
     const { t } = useTranslation();
     const {
       id,
@@ -32,75 +51,107 @@ const SelectedAllergyItem: React.FC<SelectedAllergyItemProps> = React.memo(
       type,
       selectedSeverity,
       selectedReactions,
+      note,
       errors,
       hasBeenValidated,
     } = allergy;
     const hasSeverityError = !!(hasBeenValidated && errors.severity);
     const hasReactionsError = !!(hasBeenValidated && errors.reactions);
+    const [hasNote, setHasNote] = useState(!!note);
 
     return (
-      <Grid>
-        <Column
-          sm={4}
-          md={5}
-          lg={8}
-          xlg={8}
-          className={styles.selectedAllergyTitle}
-        >
-          {display} [{t(getCategoryDisplayName(type))}]
-        </Column>
-        <Column
-          sm={4}
-          md={3}
-          lg={3}
-          xlg={3}
-          className={styles.selectedAllergySeverity}
-        >
-          <Dropdown
-            id={`allergy-severity-dropdown-${id}`}
-            data-testid={`allergy-severity-dropdown-${id}`}
-            type="default"
-            titleText={t('ALLERGY_SELECT_SEVERITY')}
-            hideLabel
-            label={t('ALLERGY_SELECT_SEVERITY')}
-            items={ALLERGY_SEVERITY_CONCEPTS}
-            selectedItem={selectedSeverity}
-            itemToString={(item) => t(item!.display!)}
-            onChange={(data) => {
-              updateSeverity(id, data.selectedItem);
+      <>
+        <Grid>
+          <Column
+            sm={4}
+            md={5}
+            lg={8}
+            xlg={8}
+            className={styles.selectedAllergyTitle}
+          >
+            {display} [{t(getCategoryDisplayName(type))}]
+            {!hasNote && (
+              <Link
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setHasNote(true);
+                }}
+                className={styles.addAllergyNote}
+              >
+                {t('ADD_ALLERGY_NOTE')}
+              </Link>
+            )}
+          </Column>
+          <Column
+            sm={4}
+            md={3}
+            lg={3}
+            xlg={3}
+            className={styles.selectedAllergySeverity}
+          >
+            <Dropdown
+              id={`allergy-severity-dropdown-${id}`}
+              data-testid={`allergy-severity-dropdown-${id}`}
+              type="default"
+              titleText={t('ALLERGY_SELECT_SEVERITY')}
+              hideLabel
+              label={t('ALLERGY_SELECT_SEVERITY')}
+              items={ALLERGY_SEVERITY_CONCEPTS}
+              selectedItem={selectedSeverity}
+              itemToString={(item) => t(item!.display!)}
+              onChange={(data) => {
+                updateSeverity(id, data.selectedItem);
+              }}
+              invalid={hasSeverityError}
+              invalidText={hasSeverityError && t(errors.severity!)}
+              autoAlign
+              aria-label={t('ALLERGY_SEVERITY_ARIA_LABEL')}
+            />
+          </Column>
+          <Column
+            sm={4}
+            md={4}
+            lg={4}
+            xlg={4}
+            className={styles.selectedAllergyReactions}
+          >
+            <FilterableMultiSelect
+              id={`allergy-reactions-multiselect-${id}`}
+              data-testid={`allergy-reactions-multiselect-${id}`}
+              type="default"
+              titleText={t('ALLERGY_SELECT_REACTIONS')}
+              hideLabel
+              placeholder={t('ALLERGY_SELECT_REACTIONS')}
+              items={reactionConcepts}
+              selectedItems={selectedReactions}
+              itemToString={(item) => (item?.display ? item.display : '')}
+              onChange={(data) => {
+                updateReactions(id, data.selectedItems!);
+              }}
+              invalid={hasReactionsError}
+              invalidText={t(errors.reactions!)}
+              autoAlign
+            />
+          </Column>
+        </Grid>
+        {hasNote && (
+          <TextAreaWClose
+            id={`allergy-note-${id}`}
+            data-testid={`allergy-note-${id}`}
+            labelText={t('ADD_ALLERGY_NOTE')}
+            placeholder={t('ADD_ALLERGY_NOTE')}
+            value={note || ''}
+            onChange={(event) => updateNote(id, event.target.value)}
+            onClose={() => {
+              setHasNote(false);
+              updateNote(id, '');
             }}
-            invalid={hasSeverityError}
-            invalidText={hasSeverityError && t(errors.severity!)}
-            autoAlign
-            aria-label={t('ALLERGY_SEVERITY_ARIA_LABEL')}
+            enableCounter
+            maxCount={1024}
           />
-        </Column>
-        <Column
-          sm={4}
-          md={4}
-          lg={4}
-          xlg={4}
-          className={styles.selectedAllergyReactions}
-        >
-          <FilterableMultiSelect
-            id={`allergy-reactions-multiselect-${id}`}
-            data-testid={`allergy-reactions-multiselect-${id}`}
-            type="default"
-            titleText={t('ALLERGY_SELECT_REACTIONS')}
-            hideLabel
-            placeholder={t('ALLERGY_SELECT_REACTIONS')}
-            items={reactionConcepts}
-            selectedItems={selectedReactions}
-            itemToString={(item) => (item?.display ? item.display : '')}
-            onChange={(data) => {
-              updateReactions(id, data.selectedItems!);
-            }}
-            invalid={hasReactionsError}
-            invalidText={t(errors.reactions!)}
-            autoAlign
-          />
-        </Column>
-      </Grid>
+        )}
+      </>
     );
   },
 );
