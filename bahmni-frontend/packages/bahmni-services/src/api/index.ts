ec12da7cab361cff6bf7672930f21a69e1567047
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-07-24 15:22:30 +0530

    Fix. Remove Unused Libs

--- bahmni-frontend/packages/bahmni-services/src/api/index.ts
+++ bahmni-frontend/packages/bahmni-services/src/api/index.ts
@@ -1,24 +1,50 @@
 import { AxiosResponse } from 'axios';
 import client from './client';
 
-export const get = async <T>(url: string): Promise<T> => {
+/**
+ * HTTP GET request
+ * @param url - The URL to send the GET request to
+ * @returns Promise resolving to the response data
+ */
+export const get = async <T = unknown>(url: string): Promise<T> => {
   const response: AxiosResponse<T> = await client.get(url);
   return response.data;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const post = async <T>(url: string, data: any): Promise<T> => {
+/**
+ * HTTP POST request
+ * @param url - The URL to send the POST request to
+ * @param data - The data to send in the request body
+ * @returns Promise resolving to the response data
+ */
+export const post = async <T = unknown, D = unknown>(
+  url: string,
+  data: D
+): Promise<T> => {
   const response: AxiosResponse<T> = await client.post(url, data);
   return response.data;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const put = async <T>(url: string, data: any): Promise<T> => {
+/**
+ * HTTP PUT request
+ * @param url - The URL to send the PUT request to
+ * @param data - The data to send in the request body
+ * @returns Promise resolving to the response data
+ */
+export const put = async <T = unknown, D = unknown>(
+  url: string,
+  data: D
+): Promise<T> => {
   const response: AxiosResponse<T> = await client.put(url, data);
   return response.data;
 };
 
-export const del = async <T>(url: string): Promise<T> => {
+/**
+ * HTTP DELETE request
+ * @param url - The URL to send the DELETE request to
+ * @returns Promise resolving to the response data
+ */
+export const del = async <T = unknown>(url: string): Promise<T> => {
   const response: AxiosResponse<T> = await client.delete(url);
   return response.data;
 };
--- bahmni-frontend/packages/bahmni-services/src/index.ts
+++ bahmni-frontend/packages/bahmni-services/src/index.ts
@@ -1,2 +1,1 @@
-export * from './lib/bahmni-services';
 export { get, post, put, del } from './api';
