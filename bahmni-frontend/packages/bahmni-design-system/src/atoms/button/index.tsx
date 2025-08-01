9b06e0bebf478e3ea4644b1f3418005788fc1d48
kaviyasribathrachalam <kaviyasri.b@thoughtworks.com>
2025-08-01 16:24:38 +0530

    BN-137 | Kaviya, Dev - Remove respective separate wrapper files and use component level index files

--- bahmni-frontend/packages/bahmni-design-system/src/atoms/button/index.tsx
+++ bahmni-frontend/packages/bahmni-design-system/src/atoms/button/index.tsx
@@ -1,1 +1,39 @@
-export { Button, type ButtonProps, ButtonSet } from './Button';
+import {
+  Button as CarbonButton,
+  ButtonProps as CarbonButtonProps,
+  ButtonSet as CarbonButtonSet,
+} from '@carbon/react';
+import React from 'react';
+
+export type ButtonProps = CarbonButtonProps<'button'> & {
+  testId?: string;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <CarbonButton {...carbonProps} data-testid={testId}>
+      {children}
+    </CarbonButton>
+  );
+};
+
+export type ButtonSetProps = React.ComponentProps<typeof CarbonButtonSet> & {
+  testId?: string;
+  children?: React.ReactNode;
+};
+
+export const ButtonSet: React.FC<ButtonSetProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <CarbonButtonSet {...carbonProps} data-testid={testId}>
+      {children}
+    </CarbonButtonSet>
+  );
+};
--- bahmni-frontend/packages/bahmni-design-system/src/atoms/tabs/index.tsx
+++ bahmni-frontend/packages/bahmni-design-system/src/atoms/tabs/index.tsx
@@ -1,12 +1,109 @@
+import {
+  Tab,
+  TabProps,
+  TabList,
+  TabListProps,
+  TabPanel,
+  TabPanelProps,
+  TabPanels,
+  TabPanelsProps,
+  Tabs as CarbonTabs,
+  TabsProps as CarbonTabsProps,
+} from '@carbon/react';
+import React from 'react';
+
+export type TabsProps = CarbonTabsProps & {
+  testId?: string;
+};
+
+export const Tabs: React.FC<TabsProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <CarbonTabs {...carbonProps} data-testid={testId}>
+      {children}
+    </CarbonTabs>
+  );
+};
+
+export type TabListWrapperProps = TabListProps & {
+  testId?: string;
+  children?: React.ReactNode;
+};
+
+export const TabListWrapper: React.FC<TabListWrapperProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <TabList {...carbonProps} data-testid={testId}>
+      {children}
+    </TabList>
+  );
+};
+
+export type TabWrapperProps = TabProps & {
+  testId?: string;
+  children?: React.ReactNode;
+};
+
+export const TabWrapper: React.FC<TabWrapperProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <Tab {...carbonProps} data-testid={testId}>
+      {children}
+    </Tab>
+  );
+};
+
+export type TabPanelsWrapperProps = TabPanelsProps & {
+  testId?: string;
+  children?: React.ReactNode;
+};
+
+export const TabPanelsWrapper: React.FC<TabPanelsWrapperProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <TabPanels {...carbonProps} data-testid={testId}>
+      {children}
+    </TabPanels>
+  );
+};
+
+export type TabPanelWrapperProps = TabPanelProps & {
+  testId?: string;
+  children?: React.ReactNode;
+};
+
+export const TabPanelWrapper: React.FC<TabPanelWrapperProps> = ({
+  testId,
+  children,
+  ...carbonProps
+}) => {
+  return (
+    <TabPanel {...carbonProps} data-testid={testId}>
+      {children}
+    </TabPanel>
+  );
+};
+
+// Export with aliases for cleaner API
 export {
-  Tabs,
   TabListWrapper as TabList,
   TabWrapper as Tab,
   TabPanelsWrapper as TabPanels,
   TabPanelWrapper as TabPanel,
-  type TabsProps,
   type TabListWrapperProps as TabListProps,
   type TabWrapperProps as TabProps,
   type TabPanelsWrapperProps as TabPanelsProps,
   type TabPanelWrapperProps as TabPanelProps,
-} from './Tabs';
+};
