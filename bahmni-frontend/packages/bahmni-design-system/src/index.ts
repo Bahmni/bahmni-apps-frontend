f9798c5cdef55a5a9b784e70e98c6fb8fb9bb379
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-08-01 12:31:30 +0530

    Refactor. Export Default Types From Atoms

--- bahmni-frontend/packages/bahmni-design-system/src/index.ts
+++ bahmni-frontend/packages/bahmni-design-system/src/index.ts
@@ -1,7 +1,12 @@
 export { Button, ButtonSet, type ButtonProps } from './atoms/button';
 export { Tile, type TileProps } from './atoms/tile';
 export { Tag, type TagProps } from './atoms/tag';
-export { Icon, type IconProps, ICON_SIZE, ICON_PADDING } from './molecules/icon';
+export {
+  Icon,
+  type IconProps,
+  ICON_SIZE,
+  ICON_PADDING,
+} from './molecules/icon';
 export { initFontAwesome } from './fontawesome';
 export {
   Accordion,
@@ -9,27 +14,6 @@ export {
   type AccordionProps,
   type AccordionItemProps,
 } from './atoms/accordion';
-export {
-  DataTable,
-  DataTableSkeleton,
-  Table,
-  TableHead,
-  TableRow,
-  TableHeader,
-  TableBody,
-  TableCell,
-  TableContainer,
-  type DataTableProps,
-  type DataTableSkeletonProps,
-  type TableProps,
-  type TableHeadProps,
-  type TableRowProps,
-  type TableHeaderProps,
-  type TableBodyProps,
-  type TableCellProps,
-  type TableContainerProps,
-  type DataTableHeader,
-} from './atoms/dataTable';
 export {
   Tabs,
   TabList,
@@ -44,6 +28,4 @@ export {
 } from './atoms/tabs';
 export { NumberInput, type NumberInputProps } from './atoms/numberInput';
 export { ComboBox, type ComboBoxProps } from './atoms/comboBox';
-export { TextInput } from './atoms/textInput';
-export { TextArea } from './atoms/textArea';
-export { FormGroup } from './atoms/formGroup';
+export { TextInput, type TextInputProps } from './atoms/textInput';
