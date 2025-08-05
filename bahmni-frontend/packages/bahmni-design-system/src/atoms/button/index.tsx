c1711310410eb2b6534d140a0a5d84199d10182c
kaviyasribathrachalam <kaviyasri.b@thoughtworks.com>
2025-08-05 16:40:47 +0530

    BN-137 | Kaviya, Dev - Remove Button set from bahmni-design-system

--- bahmni-frontend/packages/bahmni-design-system/src/atoms/button/index.tsx
+++ bahmni-frontend/packages/bahmni-design-system/src/atoms/button/index.tsx
@@ -1,9 +1,7 @@
 import {
   Button as CarbonButton,
   ButtonProps as CarbonButtonProps,
-  ButtonSet as CarbonButtonSet,
 } from '@carbon/react';
-import { ButtonSetProps as CarbonButtonSetProps } from '@carbon/react/lib/components/ButtonSet/ButtonSet';
 import React from 'react';
 
 export type ButtonProps = CarbonButtonProps<'button'> & {
@@ -19,21 +17,5 @@ export const Button: React.FC<ButtonProps> = ({
     <CarbonButton {...carbonProps} data-testid={testId}>
       {children}
     </CarbonButton>
-  );
-};
-
-export type ButtonSetProps = CarbonButtonSetProps & {
-  testId?: string;
-};
-
-export const ButtonSet: React.FC<ButtonSetProps> = ({
-  testId,
-  children,
-  ...carbonProps
-}) => {
-  return (
-    <CarbonButtonSet {...carbonProps} data-testid={testId}>
-      {children}
-    </CarbonButtonSet>
   );
 };
