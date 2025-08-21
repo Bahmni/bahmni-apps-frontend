5cf0086dbc0ba34922282c34d9697033b840121d
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-08-21 07:43:18 +0530

    BN-114 | Add. Registration App Foundation

---
+++ apps/registration/src/lib/registration.tsx
@@ -0,0 +1,11 @@
+import styles from './registration.module.scss';
+
+export function BahmniFrontendRegistration() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to BahmniFrontendRegistration!</h1>
+    </div>
+  );
+}
+
+export default BahmniFrontendRegistration;
