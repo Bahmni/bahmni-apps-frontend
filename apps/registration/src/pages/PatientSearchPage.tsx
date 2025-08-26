3b0e2f48a546e81d82bc9ad1e510650e0cbd4ebd
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-08-26 07:58:12 +0530

    BN-114 | Add. Sortable Data Table Search Result View To PatientSearch Page
    Co-authored-by: Aishwarya Sontakke <aishwarya.sontakke@thoughtworks.com>

--- apps/registration/src/pages/PatientSearchPage.tsx
+++ apps/registration/src/pages/PatientSearchPage.tsx
@@ -1,4 +1,9 @@
-import { BaseLayout, Header } from '@bahmni-frontend/bahmni-design-system';
+import {
+  BaseLayout,
+  Header,
+  SortableDataTable,
+  Tile,
+} from '@bahmni-frontend/bahmni-design-system';
 import {
   BAHMNI_HOME_PATH,
   PatientSearch,
@@ -12,29 +17,74 @@ const breadcrumbItems = [
   { id: 'current', label: 'Registration', isCurrentPage: true },
 ];
 
+type PatientSearchViewModel<T> = T & {
+  id: string;
+  name: string;
+  phoneNumber: string;
+  alternatePhoneNumber: string;
+};
+
 /**
  * PatientSearchPage
- *
  * Registration Patient Search interface that let's the user search for a patient using keywords.
- *
  * @returns React component with registration search interface
  */
 const PatientSearchPage: React.FC = () => {
   const [patientSearchData, setPatientSearchData] = useState<PatientSearch[]>(
     [],
   );
-  const [error, setError] = useState<Error | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
   const handleSearchPatientUpdate = (
     data: PatientSearch[] | undefined,
-    error: Error | null,
-    isLoading: boolean,
+    searchTerm: string,
   ) => {
-    setIsLoading(isLoading);
-    setError(error);
     setPatientSearchData(data ?? []);
+    setSearchTerm(searchTerm);
   };
+
+  const patientSearchResult: PatientSearchViewModel<PatientSearch>[] =
+    patientSearchData.map((item, index) => ({
+      ...item,
+      id: index.toString(),
+      name: [item.givenName, item.middleName, item.familyName].join(' '),
+      phoneNumber: item.customAttribute
+        ? JSON.parse(item.customAttribute)['phoneNumber']
+        : '',
+      alternatePhoneNumber: item.customAttribute
+        ? JSON.parse(item.customAttribute)['alternatePhoneNumber']
+        : '',
+    }));
+
+  const headers = [
+    { key: 'id', header: 'Patient ID' },
+    { key: 'name', header: 'Patient Name' },
+    { key: 'gender', header: 'Gender' },
+    { key: 'age', header: 'Age' },
+    { key: 'phoneNumber', header: 'Phone Number' },
+    { key: 'alternatePhoneNumber', header: 'Alternate Phone Number' },
+  ];
+
+  const renderCell = (
+    row: PatientSearchViewModel<PatientSearch>,
+    key: string,
+  ) => {
+    switch (key) {
+      case 'id':
+        return row.identifier;
+      case 'name':
+        return row.name;
+      case 'gender':
+        return row.gender;
+      case 'age':
+        return row.age;
+      case 'phoneNumber':
+        return row.phoneNumber;
+      case 'alternatePhoneNumber':
+        return row.alternatePhoneNumber;
+    }
+  };
+
   return (
     <BaseLayout
       header={
@@ -43,7 +93,34 @@ const PatientSearchPage: React.FC = () => {
           ariaLabel="registration-search-page-header"
         />
       }
-      main={<div className={styles.main}></div>}
+      main={
+        <div className={styles.main}>
+          <SearchPatient
+            emptyStateMessage={`Could not find patient with identifier/name ${searchTerm}`}
+            errorMessage={
+              'An unexpected error occurred during search. Please try again later.'
+            }
+            handleSearchPatient={handleSearchPatientUpdate}
+          />
+          {patientSearchResult && patientSearchResult.length > 0 && (
+            <Tile
+              id="patient-search-result"
+              aria-label="patient-search-result"
+              className={styles.patientSearchTable}
+            >
+              <p className={styles.title}>
+                Patient results ({patientSearchResult.length})
+              </p>
+              <SortableDataTable
+                headers={headers}
+                ariaLabel="patient-search-sortable-data-table"
+                rows={patientSearchResult}
+                renderCell={renderCell}
+              />
+            </Tile>
+          )}
+        </div>
+      }
     />
   );
 };
--- apps/registration/src/pages/__tests__/PatientSearchPage.test.tsx
+++ apps/registration/src/pages/__tests__/PatientSearchPage.test.tsx
@@ -1,25 +1,198 @@
-import { BAHMNI_HOME_PATH } from '@bahmni-frontend/bahmni-services';
-import { render, screen } from '@testing-library/react';
+import { PatientSearch } from '@bahmni-frontend/bahmni-services';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import PatientSearchPage from '../PatientSearchPage';
 
+const mockSearchPatientData: PatientSearch[] = [
+  {
+    uuid: '02f47490-d657-48ee-98e7-4c9133ea168b',
+    birthDate: new Date(-17366400000),
+    extraIdentifiers: null,
+    personId: 9,
+    deathDate: null,
+    identifier: 'ABC200001',
+    addressFieldValue: null,
+    givenName: 'Steffi',
+    middleName: 'Maria',
+    familyName: 'Graf',
+    gender: 'F',
+    dateCreated: new Date(1739872641000),
+    activeVisitUuid: 'de947029-15f6-4318-afff-a1cbce3593d2',
+    customAttribute: JSON.stringify({
+      phoneNumber: '864579392',
+      alternatePhoneNumber: '4596781239',
+    }),
+    hasBeenAdmitted: true,
+    age: '56',
+  },
+  {
+    uuid: '02f47490-d657-48ee-98e7-4c9133ea1685',
+    birthDate: new Date(-17366400000),
+    extraIdentifiers: null,
+    personId: 9,
+    deathDate: null,
+    identifier: 'ABC200002',
+    addressFieldValue: null,
+    givenName: 'John',
+    middleName: '',
+    familyName: 'Doe',
+    gender: 'M',
+    dateCreated: new Date(1739872641000),
+    activeVisitUuid: 'de947029-15f6-4318-afff-a1abce3593d2',
+    customAttribute: '',
+    hasBeenAdmitted: true,
+    age: '56',
+  },
+  {
+    uuid: '02f47490-d657-48ee-98e7-4c9133ea2685',
+    birthDate: new Date(-17366400000),
+    extraIdentifiers: null,
+    personId: 9,
+    deathDate: null,
+    identifier: 'ABC200003',
+    addressFieldValue: null,
+    givenName: 'Jane',
+    middleName: '',
+    familyName: 'Doe',
+    gender: 'F',
+    dateCreated: new Date(1739872641000),
+    activeVisitUuid: 'de947029-15f6-4318-afff-a1cbcs3593d2',
+    customAttribute: '',
+    hasBeenAdmitted: true,
+    age: '56',
+  },
+];
+
+jest.mock('@tanstack/react-query', () => ({
+  ...jest.requireActual('@tanstack/react-query'),
+  useQuery: jest.fn(),
+}));
+
 describe('PatientSearchPage', () => {
+  let queryClient: QueryClient;
+
   beforeEach(() => {
     jest.clearAllMocks();
+    (useQuery as jest.Mock).mockReturnValue({
+      data: undefined,
+      error: null,
+      isLoading: false,
+    });
+    Object.defineProperty(document, 'cookie', {
+      writable: true,
+      value: 'bahmni.user.location=location;',
+    });
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+  });
+
+  afterEach(() => {
+    queryClient.clear();
   });
 
   it('should render the Header with Breadcrumbs component', () => {
-    render(<PatientSearchPage />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <PatientSearchPage />
+      </QueryClientProvider>,
+    );
     expect(
       screen.getByRole('banner', { name: 'registration-search-page-header' }),
     ).toBeInTheDocument();
     expect(screen.getByText('Home')).toBeInTheDocument();
     expect(screen.getByText('Registration')).toBeInTheDocument();
+    expect(screen.getByTestId('search-patient-tile')).toBeInTheDocument();
+    expect(screen.getByTestId('search-patient-seachbar')).toBeInTheDocument();
+    expect(screen.getByTestId('search-patient-seachbar')).toHaveAttribute(
+      'placeholder',
+      'Search by name or patient ID',
+    );
   });
 
-  it('should render breadcumb with Home page link', async () => {
-    render(<PatientSearchPage />);
-    const homeButton = screen.getByRole('link', { name: 'Home' });
-    expect(homeButton).toBeInTheDocument();
-    expect(homeButton).toHaveAttribute('href', BAHMNI_HOME_PATH);
+  it('should render only search patient widget on mount', async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <PatientSearchPage />
+      </QueryClientProvider>,
+    );
+
+    expect(screen.getByTestId('search-patient-seachbar')).toHaveAttribute(
+      'placeholder',
+      'Search by name or patient ID',
+    );
+    expect(screen.queryByTestId(/sortable-table-/)).not.toBeInTheDocument();
+  });
+
+  it('should show patient details when search is successfull', async () => {
+    (useQuery as jest.Mock).mockReturnValue({
+      data: mockSearchPatientData,
+      error: null,
+      isLoading: false,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <PatientSearchPage />
+      </QueryClientProvider>,
+    );
+
+    expect(screen.getByTestId('search-patient-seachbar')).toHaveAttribute(
+      'placeholder',
+      'Search by name or patient ID',
+    );
+    const searchInput = screen.getByPlaceholderText(
+      'Search by name or patient ID',
+    );
+
+    fireEvent.input(searchInput, { target: { value: 'new value' } });
+    fireEvent.click(screen.getByTestId('search-patient-search-button'));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          'Patient results (' + mockSearchPatientData.length + ')',
+        ),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('should show not show sortable table when patient search returns no match', async () => {
+    (useQuery as jest.Mock).mockReturnValue({
+      data: undefined,
+      error: null,
+      isLoading: false,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <PatientSearchPage />
+      </QueryClientProvider>,
+    );
+
+    expect(screen.getByTestId('search-patient-seachbar')).toHaveAttribute(
+      'placeholder',
+      'Search by name or patient ID',
+    );
+    const searchInput = screen.getByPlaceholderText(
+      'Search by name or patient ID',
+    );
+
+    fireEvent.input(searchInput, { target: { value: 'ABC20000' } });
+    fireEvent.click(screen.getByTestId('search-patient-search-button'));
+
+    await waitFor(() => {
+      expect(
+        screen.queryByRole('div', { name: 'Add to cart' }),
+      ).not.toBeInTheDocument();
+    });
   });
 });
