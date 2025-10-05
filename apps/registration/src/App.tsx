f5fd41a8f9b2ad17a5e336a3b1565b551aaa4b3d
Dev Singh <88476780+devsingh05298@users.noreply.github.com>
2025-10-05 11:40:29 +0530

    BN-124|Add. Mutation to create Patient and rename folder to createPatient

--- apps/registration/src/App.tsx
+++ apps/registration/src/App.tsx
@@ -15,7 +15,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { queryClientConfig } from './config/tanstackQuery';
-import CreatePatient from './pages/newPatientRegistrationPage/createPatient';
+import CreatePatient from './pages/createPatientPage/createPatient';
 import PatientSearchPage from './pages/patientSearchPage';
 
 const queryClient = new QueryClient(queryClientConfig);
