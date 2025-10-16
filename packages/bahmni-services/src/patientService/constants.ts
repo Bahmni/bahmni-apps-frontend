7235d5fb59888c0de2b3a952cc48d79ed5bf7444
Varun Prasad <varun.prasad@thoughtworks.com>
2025-10-16 21:34:42 +0530

    BN-173|Fix. Lint fix

--- packages/bahmni-services/src/patientService/constants.ts
+++ packages/bahmni-services/src/patientService/constants.ts
@@ -103,7 +103,6 @@ export const MAX_PAGE_SIZE = 100;
 export const MAX_PATIENT_AGE_YEARS = 150;
 export const UUID_PATTERN = /^[a-f0-9-]{36}$/i;
 
-
 export const VISIT_TYPES_URL = () =>
   OPENMRS_REST_V1 +
   `/bahmnicore/config/bahmniencounter?callerContext=REGISTRATION_CONCEPTS`;
