3deadabb7a4075fabf7bc11cef3183005270eaa3
skjameela276 <jameela.shaik@thoughtworks.com>
2025-12-03 15:13:09 +0530

    BAH-4317 | Add. eoc encounter url into constant file

--- packages/bahmni-services/src/encounterService/constants.ts
+++ packages/bahmni-services/src/encounterService/constants.ts
@@ -2,3 +2,7 @@ import { OPENMRS_FHIR_R4 } from '../constants/app';
 
 export const PATIENT_VISITS_URL = (patientUUID: string) =>
   OPENMRS_FHIR_R4 + `/Encounter?subject:Patient=${patientUUID}&_tag=visit`;
+
+export const EOC_ENCOUNTERS_URL = (eocIds: string) =>
+  OPENMRS_FHIR_R4 +
+  `/EpisodeOfCare?_revincludes=Encounter:episode-of-care&_id=${eocIds}`;
