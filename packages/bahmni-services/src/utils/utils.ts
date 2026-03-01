ce9670ff1e49f52d6dec59aa3f8bc7fd73b6a84e
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2026-03-01 11:53:42 +0530

    BAH-4494 | Add. Basic Auth Service With Logout Fn

--- packages/bahmni-services/src/utils/utils.ts
+++ packages/bahmni-services/src/utils/utils.ts
@@ -354,9 +354,9 @@ export const getValueType = (value: string | number): string => {
  */
 export function camelToScreamingSnakeCase(str: string): string {
   return str
-    .replace(/[\s-]+/g, '_')
-    .replace(/([a-z])([A-Z])/g, '$1_$2')
-    .replace(/([A-Z])([A-Z][a-z])/g, '$1_$2')
+    .replaceAll(/\s+/g, '_')
+    .replaceAll(/([a-z])([A-Z])/g, '$1_$2')
+    .replaceAll(/([A-Z])([A-Z][a-z])/g, '$1_$2')
     .toUpperCase();
 }
 
