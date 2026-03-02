6710509efd60da747d295004bae95fc73fd511d4
sahupadma <padma.sahu@thoughtworks.com>
2026-03-02 14:54:39 +0530

    BAH-4413| Refactor. fix tests resize observer

--- apps/registration/setupTests.ts
+++ apps/registration/setupTests.ts
@@ -9,3 +9,10 @@ global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
 initFontAwesome();
+
+// Mock ResizeObserver for tests
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
--- distro/setupTests.ts
+++ distro/setupTests.ts
@@ -5,3 +5,10 @@ import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 // @ts-expect-error - Ignoring type issues with Node.js util TextDecoder
 global.TextDecoder = TextDecoder;
+
+// Mock ResizeObserver for tests
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
