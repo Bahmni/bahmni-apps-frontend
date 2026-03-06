c8010f1fde64e00be9130adb906460a5d95f117c
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2026-03-06 15:54:24 +0530

    BAH-4511 | Add. Author And Other Metadata To package.json

--- scripts/create-app.js
+++ scripts/create-app.js
@@ -85,7 +85,6 @@ function getPackageJsonTemplate(appNameKebab) {
       exports: {
         './package.json': './package.json',
         '.': {
-          '@bahmni/source': './src/index.ts',
           types: './dist/index.d.ts',
           import: './dist/index.js',
           default: './dist/index.js',
@@ -98,6 +97,13 @@ function getPackageJsonTemplate(appNameKebab) {
         'react-dom': '^19.0.0',
         'react-router-dom': '^7.5.3',
       },
+      "author": "Thoughtworks Inc.",
+      "license": "MPLv2",
+      "files": [
+        "dist",
+        "README.md",
+        "LICENSE"
+      ]
     },
     null,
     2,
