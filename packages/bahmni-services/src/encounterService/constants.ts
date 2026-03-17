27faa31da995367b6cf2f967df99370f673409a0
Aishwarya-thoughtworks <aishwarya.sontakke@thoughtworks.com>
2026-03-17 21:10:41 +0530

    BAH-4487|Fix.Rename constant and update test

--- packages/bahmni-services/src/encounterService/constants.ts
+++ packages/bahmni-services/src/encounterService/constants.ts
@@ -3,5 +3,7 @@ import { OPENMRS_FHIR_R4 } from '../constants/app';
 export const PATIENT_VISITS_URL = (patientUUID: string) =>
   OPENMRS_FHIR_R4 + `/Encounter?subject:Patient=${patientUUID}&_tag=visit`;
 
-export const BAHMNI_ENCOUNTER_URL = (encounterUUID: string) =>
+// Fetches all observations for an encounter using custom Bahmni FHIR operation
+// (not standard R4). Requires bahmni-module-fhir2-addl-extension.
+export const FHIR_OBSERVATIONS_BY_ENCOUNTER_URL = (encounterUUID: string) =>
   `${OPENMRS_FHIR_R4}/Observation/$everything-by-encounter?encounter=${encounterUUID}`;
