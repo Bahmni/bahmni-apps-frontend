0ed75a850bae4a9da1f1486269062768dcf68755
Aishwarya-thoughtworks <aishwarya.sontakke@thoughtworks.com>
2026-03-18 17:17:49 +0530

    BAH-4487|Refactor. rename url

--- packages/bahmni-services/src/encounterService/constants.ts
+++ packages/bahmni-services/src/encounterService/constants.ts
@@ -4,4 +4,4 @@ export const PATIENT_VISITS_URL = (patientUUID: string) =>
   OPENMRS_FHIR_R4 + `/Encounter?subject:Patient=${patientUUID}&_tag=visit`;
 
 export const FHIR_OBSERVATIONS_BY_ENCOUNTER_URL = (encounterUUID: string) =>
-  `${OPENMRS_FHIR_R4}/Observation/$everything-by-encounter?encounter=${encounterUUID}`;
+  `${OPENMRS_FHIR_R4}/Observation/$fetch-all?encounter=${encounterUUID}`;
