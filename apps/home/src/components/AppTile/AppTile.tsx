5662149edee1d36e1acd0ebe98ad3110b6982799
arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>
2026-05-26 16:45:26 +0530

    BAH-4668. Use h2 instead of p in AppTile.tsx

--- apps/home/src/components/AppTile/AppTile.tsx
+++ apps/home/src/components/AppTile/AppTile.tsx
@@ -26,9 +26,9 @@ export const AppTile: React.FC<AppTileProps> = ({ id, label, icon, url }) => {
       aria-label={translatedLabel}
       testId={`app-tile-${id}`}
     >
-      <p className={styles.label} aria-hidden="true">
+      <h2 className={styles.label} aria-hidden="true">
         {translatedLabel}
-      </p>
+      </h2>
       <div className={styles.bottom}>
         <Icon name={icon} id={id} size={ICON_SIZE.X2} aria-hidden="true" />
         <ArrowRight size={20} aria-hidden="true" />
