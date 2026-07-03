878787a57196caaa54e1807fa23ac9e16df841bd
Dev Singh <88476780+devsingh05298@users.noreply.github.com>
2026-07-03 15:57:14 +0530

    BAH-4842|Fix. the url for BAHMNI_CLINICAL_PATH

--- apps/clinical/src/constants/app.ts
+++ apps/clinical/src/constants/app.ts
@@ -1,7 +1,7 @@
 import { OPENMRS_REST_V1, OPENMRS_FHIR_R4 } from '@bahmni/services';
 
 export const BAHMNI_CLINICAL_PATH =
-  '/bahmni/clinical/index.html#/v2/patient/search';
+  '/bahmni/clinical/index.html#/default/patient/search';
 export const ENCOUNTER_CONCEPTS_URL =
   OPENMRS_REST_V1 +
   '/bahmnicore/config/bahmniencounter?callerContext=REGISTRATION_CONCEPTS';
